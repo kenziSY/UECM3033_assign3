daf38997544bb610deea1493451a148a1558517c
kenziSY <kenziSY@1utar.my>
2016-04-17 18:45:11 +0800

    Modified task1.py

--- task1.py
+++ task1.py
@@ -1,13 +1,14 @@
 import numpy as np
 import sympy as sy
-#Your optional code here
-#You can import some modules or create additional functions
-
 # DO NOT CHANGE THE NAME OF gausslegendre() function
-def gausslegendre(f, a, b, n=20):
-    ans = 0
-    # Edit here to implement your code
 
+def gausslegendre(f, a, b, n=20):
+    
+    ans = 0  
+    [X,W]=np.polynomial.legendre.leggauss(n)       
+    y=(0.5*(b-a))*X+(0.5*(a+b))
+    ans=(0.5*(b-a))*sum(W*f(y))
+    
     return ans
 
 if __name__ == "__main__":
